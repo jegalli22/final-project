49fe1ea3c2651666c5b6277bdecab55815b84edf
lfleming-314 <68925502+lfleming-314@users.noreply.github.com>
2020-10-14 14:37:16 -0400

    v2

--- scheduling.js
+++ scheduling.js
@@ -1,3 +1,12 @@
-function buildAvailabilityTable() {
-    
+function buildAvailTable(tableData) {
+    var atable = "<table>";
+    for (i=0; i<tableData.length; i++) {
+        atable += "<tr>";
+        for (k=0; k<tableData[i].length; k++) {
+            atable += "<td>"+tableData[i][k]+"</td>";
+        }
+        atable += "</tr>";
+    }
+    atable += "</table>";
+    document.getElementById("availTable").innerHTML = atable;
 }
